004fa4b9a7f07861ecee2ab1ceec5c3d5fe4b434
mikelove <michaelisaiahlove@gmail.com>
2018-03-08 14:35:50 -0500

    tweaks

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -28,12 +28,18 @@ suppressPackageStartupMessages(library(DESeq2))
 dds <- DESeqDataSetFromTximport(txi, coldata, ~condition)
 
 # remove failing samples
+table(dds$condition)
 qcFail <- c("SRR1813874","SRR1813893")
 qcFail %in% colnames(dds)
-dim(dds)
 dds <- dds[,!colnames(dds) %in% qcFail]
-dim(dds)
 table(dds$condition)
+
+# remove other samples
+otherRemove <- c("SRR1813895","SRR1813889")
+otherRemove %in% colnames(dds)
+dds <- dds[,!colnames(dds) %in% otherRemove]
+table(dds$condition)
+
 
 vsd <- vst(dds)
 plotPCA(vsd)
@@ -41,30 +47,28 @@ plotPCA(vsd, "Sex_s")
 plotPCA(vsd, "age_s")
 plotPCA(vsd, "MBases_l")
 
-suppressPackageStartupMessages(library("sva"))
-dds <- estimateSizeFactors(dds)
-dat  <- counts(dds, normalized=TRUE)
-idx  <- rowMeans(dat) > 1
-dat  <- dat[idx, ]
-mod  <- model.matrix(~ condition, colData(dds))
-mod0 <- model.matrix(~   1, colData(dds))
-fit <- svaseq(dat, mod, mod0, n.sv = 2)
-
-dds$sv1 <- fit$sv[,1]
-dds$sv2 <- fit$sv[,2]
-vsd$sv1 <- fit$sv[,1]
-vsd$sv2 <- fit$sv[,2]
-
-plotPCA(vsd, "sv1")
-plotPCA(vsd, "sv2")
+## suppressPackageStartupMessages(library("sva"))
+## dds <- estimateSizeFactors(dds)
+## dat  <- counts(dds, normalized=TRUE)
+## idx  <- rowMeans(dat) > 1
+## dat  <- dat[idx, ]
+## mod  <- model.matrix(~ condition, colData(dds))
+## mod0 <- model.matrix(~   1, colData(dds))
+## fit <- svaseq(dat, mod, mod0, n.sv = 2)
+## dds$sv1 <- fit$sv[,1]
+## dds$sv2 <- fit$sv[,2]
+## vsd$sv1 <- fit$sv[,1]
+## vsd$sv2 <- fit$sv[,2]
+## plotPCA(vsd, "sv1")
+## plotPCA(vsd, "sv2")
 
 keep <- rowSums(counts(dds, normalized=TRUE) >= 5) >= 5
 table(keep)
 dds <- dds[keep,]
 
 # takes 1.5 min
-design(dds) <- ~sv1 + sv2 + condition
-system.time({ dds <- DESeq(dds, test="LRT", reduced=~sv1 + sv2, minReplicatesForReplace=Inf) })
+design(dds) <- ~condition
+system.time({ dds <- DESeq(dds, test="LRT", reduced=~1, minReplicatesForReplace=Inf) })
 res <- results(dds, alpha=.05)
 summary(res)
 
@@ -73,3 +77,13 @@ for (i in 1:9)
   plotCounts(dds, gene=order(res$pvalue)[i], transform=FALSE)
 
 rownames(res)[order(res$pvalue)[1:9]]
+
+library(org.Hs.eg.db)
+unname(
+  mapIds(org.Hs.eg.db,
+       sub("\\..*","",rownames(res)[order(res$pvalue)[1:50]]),
+       "SYMBOL",
+       "ENSEMBL")
+)
+
+# Ben # normal samples vs 
